e0f876c3b8997649d6bedbd6e4f6c341fcef829c
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-02-25 22:08:05 -0300

    Customize inital configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Amanda Mota Dermatologista`,
+    description: `Dermatologista formada pela UERJ, com título de especialista pela SBD, atua no consultorio privado e como professora na residencia em dermatologia do HCA.`,
+    author: `@arturfsousa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,12 +19,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Amanda Mota`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#80a1d4`,
+        theme_color: `#80a1d4`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/website-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

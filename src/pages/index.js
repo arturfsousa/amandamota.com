b940d3e8419e7c174d1559ca76adf6db7ceea7cb
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-02-26 20:31:21 -0300

    Add a schedule button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import React from "react"
-
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import bgHome from "../images/bg-home.png"
+import btSchedule from "../images/bt-schedule.png"
+
+const scheduleButton = css`
+  position: absolute;
+  top: 465px;
+  left: 0;
+`
 
 const IndexPage = () => (
   <Layout bgImage={bgHome}>
     <SEO />
-    Hello man
+    <a href="#" css={scheduleButton}>
+      <img src={btSchedule} alt="Agende uma consulta" />
+    </a>
   </Layout>
 )
 

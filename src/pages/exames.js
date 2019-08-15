0d13c1fa6beff32440da77dc29fe61b21b1b3f5e
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-08-15 20:14:59 -0300

    Add some Exames

--- src/pages/exames.js
+++ src/pages/exames.js
@@ -4,10 +4,73 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import Head from "../components/Head"
 
+import Gallery from "../components/Gallery"
+import GalleryGroup from "../components/GalleryGroup"
+import GalleryItem from "../components/GalleryItem"
+
+import treatment1 from "../images/treatment-1.png"
+import treatment2 from "../images/treatment-2.png"
+import treatment3 from "../images/treatment-3.png"
+
+const galleryItems = [
+  [
+    {
+      title: "Skin Booster",
+      description:
+        "Hidratação profunda para maior suavidade, elasticidade, firmeza e melhora da aparência da pele.",
+      imageUrl: treatment1,
+    },
+    {
+      title: "Luz Intensa Pulsada",
+      description:
+        "Reduz manchas solares, sardas, trata rugas, poros dilatados e mudanças na textura da pele.",
+      imageUrl: treatment2,
+    },
+    {
+      title: "Preenchimento",
+      description:
+        "Para repor volume facial, dos lábios, contorno facial e amenizar bigode chinês, olheiras e rugas.",
+      imageUrl: treatment3,
+    },
+    {
+      title: "Skin Booster",
+      description:
+        "Hidratação profunda para maior suavidade, elasticidade, firmeza e melhora da aparência da pele.",
+      imageUrl: treatment1,
+    },
+    {
+      title: "Luz Intensa Pulsada",
+      description:
+        "Reduz manchas solares, sardas, trata rugas, poros dilatados e mudanças na textura da pele.",
+      imageUrl: treatment2,
+    },
+    {
+      title: "Preenchimento",
+      description:
+        "Para repor volume facial, dos lábios, contorno facial e amenizar bigode chinês, olheiras e rugas.",
+      imageUrl: treatment3,
+    },
+  ],
+]
+
 const ExamesPage = () => (
   <Layout>
     <SEO title="Exames" />
     <Head>Exames</Head>
+    <Gallery>
+      {galleryItems.map((group, i) => (
+        <GalleryGroup key={`gallery-group-${i}`}>
+          {group.map(item => (
+            <GalleryItem
+              key={item.title}
+              title={item.title}
+              description={item.description}
+              imageUrl={item.imageUrl}
+            />
+          ))}
+        </GalleryGroup>
+      ))}
+    </Gallery>
   </Layout>
 )
 

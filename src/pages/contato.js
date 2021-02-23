d0def2c6ec1728bdc2ca1da5d78b96db6e2150ef
Artur Sousa <artur.sousa@skyscanner.net>
2021-02-23 20:39:12 +0100

    Update phone and address

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -36,7 +36,7 @@ const ContatoPage = () => (
     <section css={section}>
       <p>
         <PhoneAndroid />
-        <span>(21) 2512-8470 | (21) 98889-5939</span>
+        <span>(21) 99539-2015</span>
       </p>
       <p>
         <MailOutline />
@@ -45,7 +45,13 @@ const ContatoPage = () => (
       <p>
         <Business />
         <span>
-          Rua General Garzon, 22 - Sala 403 - Jd. Botânico | CEP: 22470-010
+          <a
+            href="https://www.google.com/maps/place/R.+Jardim+Bot%C3%A2nico,+674+-+sala+207+-+Jardim+Bot%C3%A2nico,+Rio+de+Janeiro+-+RJ,+22461-000,+Brazil/@-22.9645794,-43.2205535,17z/data=!3m1!4b1!4m5!3m4!1s0x9bd59953155371:0xd85dfbee5e9fa563!8m2!3d-22.9645844!4d-43.2183648"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rua Jardim Botânico 674 Sala 207, Rio De Janeiro, RJ, Brasil
+          </a>
         </span>
       </p>
     </section>
